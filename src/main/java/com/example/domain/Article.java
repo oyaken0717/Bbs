d5941883d3db37e6656c1cb243afe0d91947b55f
yusukeAshibe <yusuke.ashibe@rakus-partners.co.jpgit config --listgit config --global user.email yusuke.ashibe@rakus-partners.co.jp>
2020-02-28 13:37:04 +0900

    Articleドメインの新規作成

---
+++ src/main/java/com/example/domain/Article.java
@@ -0,0 +1,71 @@
+package com.example.domain;
+
+import java.util.List;
+
+/**
+ * Articleを表すドメインクラス.
+ * @author ashibe
+ *
+ */
+public class Article {
+	
+	/**
+	 * id
+	 */
+	private Integer id;
+	
+	/**
+	 * 名前
+	 */
+	private String name;
+	
+	/**
+	 * コメント
+	 */
+	private String Content;
+	
+	/**
+	 * コメントリスト
+	 */
+	private List<Comment> commentList;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return Content;
+	}
+
+	public void setContent(String content) {
+		Content = content;
+	}
+
+	public List<Comment> getCommentList() {
+		return commentList;
+	}
+
+	public void setCommentList(List<Comment> commentList) {
+		this.commentList = commentList;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", name=" + name + ", Content=" + Content + ", commentList=" + commentList + "]";
+	}
+	
+	
+
+}
